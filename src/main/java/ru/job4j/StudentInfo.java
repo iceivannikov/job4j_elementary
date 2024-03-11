4f29e02ecdf5457ef213dabbfd34fd45ac545aea
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-11 19:53:26 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Viktor Ivannikov");
+        System.out.println("17.01.1985");
+    }
+}
