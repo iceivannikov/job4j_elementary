dbffbf87dd0f9f67083984c588c3887ab612ff3e
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-20 14:40:48 +0300

    6.0.7. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -10,7 +10,7 @@ public class Square {
     }
 
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(3);
         for (int i : array) {
             System.out.println(i);
         }
